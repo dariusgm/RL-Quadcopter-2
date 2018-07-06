8b2d7061e019adf0a92adcb6f2b37215d0645116
dariusgm <dariusmurawski@hotmail.com>
2018-07-06 20:34:00 +0200

    Solution Quadcopter

--- task.py
+++ task.py
@@ -1,5 +1,67 @@
 import numpy as np
 from physics_sim import PhysicsSim
+import math
+
+class TaskFlyAway():
+    """
+    Task (environment) that defines the goal and provides feedback to the agent.
+    This task wants the agent to escape as far as possible within the runtime.
+    """
+    def __init__(self, init_pose=None, init_velocities=None, 
+        init_angle_velocities=None, runtime=1.):
+        """Initialize a Task object.
+        Params
+        ======
+            init_pose: initial position of the quadcopter in (x,y,z) dimensions and the Euler angles
+            init_velocities: initial velocity of the quadcopter in (x,y,z) dimensions
+            init_angle_velocities: initial radians/second for each of the three Euler angles
+            runtime: time limit for each episode
+        """
+        # Simulation
+        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
+        self.action_repeat = 3
+
+        self.state_size = self.action_repeat * 6
+        self.action_low = 0
+        self.action_high = 900
+        self.action_size = 4
+        
+        # Goal, maximize this distance
+        self.init_pose = init_pose
+      
+
+    def get_reward(self):
+        """Uses Distance from init_pose to the current pose as distance. As more far away, as more reward"""
+        # Formula from: https://www.varsitytutors.com/hotmath/hotmath_help/topics/distance-formula-in-3d
+        delta_x = self.init_pose[0] - self.sim.pose[0]
+        delta_y = self.init_pose[1] - self.sim.pose[1]
+        delta_z = self.init_pose[2] - self.sim.pose[2]
+        
+        delta_x_pow = math.pow(delta_x,2)
+        delta_y_pow = math.pow(delta_y,2)
+        delta_z_pow = math.pow(delta_z,2)
+        distance = math.sqrt(delta_x_pow + delta_y_pow + delta_z_pow)
+
+        return distance
+
+    
+    def step(self, rotor_speeds):
+        """Uses action to obtain next state, reward, done."""
+        reward = 0
+        pose_all = []
+
+        for _ in range(self.action_repeat):
+            done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
+            reward += self.get_reward()
+            pose_all.append(self.sim.pose)
+        next_state = np.concatenate(pose_all)
+        return next_state, reward, done
+
+    def reset(self):
+        """Reset the sim to start a new episode."""
+        self.sim.reset()
+        state = np.concatenate([self.sim.pose] * self.action_repeat) 
+        return state
 
 class Task():
     """Task (environment) that defines the goal and provides feedback to the agent."""
